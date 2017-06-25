d0e6a7132a757e1ebf1b0a6c747f65373aac5574
ulyanov yuriy <ulyanov.yuriy@live.com>
2017-06-25 21:11:15 +0300

    db lock

--- core/DocStore.Core/Lock/DbLock.cs
+++ core/DocStore.Core/Lock/DbLock.cs
@@ -11,6 +11,10 @@ namespace DocStore.Core.Lock
     /// </summary>
     public class DbLock : ILock
     {
+        private static TimeSpan Min = TimeSpan.FromSeconds(2);
+
+        private static TimeSpan Max = TimeSpan.FromHours(1);
+
         /// <summary>
         /// Транзакция
         /// </summary>
@@ -56,7 +60,67 @@ namespace DocStore.Core.Lock
             , string owner = null
             , TimeSpan? timeout = default(TimeSpan?))
         {
-            return LockResult.Granted;
+            timeout = timeout ?? Max;
+            if (timeout < Min)
+                timeout = Min;
+
+            if (mode == LockMode.Exclusive)
+            {
+                return GetXLock(resource, owner, timeout.Value);
+            }
+            else if (mode == LockMode.Shared)
+            {
+                return GetSLock(resource, owner, timeout.Value);
+            }
+            else throw new NotSupportedException(mode.ToString());
+        }
+
+        private LockResult GetXLock(string resource, string owner, TimeSpan value)
+        {
+            using (var cmd = Conn.CreateCommand())
+            {
+                cmd.Transaction = Tran;
+                cmd.CommandTimeout = (int)value.TotalSeconds;
+
+                cmd.CommandText = $"INSERT INTO XLOCK (RESOURCE, OWNER) VALUES('{resource}', '{owner}')" +
+                    $";DELETE FROM SLOCK WHERE RESOURCE = '{resource}'";
+
+                try
+                {
+                    var r = cmd.ExecuteNonQuery();
+                    if (r == 1)
+                        return LockResult.Granted;
+                    else return LockResult.GrantedAfterWait;
+                }
+                catch (Exception e)
+                {
+                    return LockResult.Timeouted;
+                }
+            }
+        }
+
+        private LockResult GetSLock(string resource, string owner, TimeSpan value)
+        {
+            using (var cmd = Conn.CreateCommand())
+            {
+                cmd.Transaction = Tran;
+                cmd.CommandTimeout = (int)value.TotalSeconds;
+
+                cmd.CommandText = $"DELETE FROM XLOCK WHERE RESOURCE = '{resource}'" +
+                    $";INSERT INTO SLOCK (RESOURCE, OWNER) VALUES('{resource}', '{owner}')";
+
+                try
+                {
+                    var r = cmd.ExecuteNonQuery();
+                    if (r == 1)
+                        return LockResult.Granted;
+                    else return LockResult.GrantedAfterWait;
+                }
+                catch (Exception e)
+                {
+                    return LockResult.Timeouted;
+                }
+            }
         }
 
         #region IDisposable Support
